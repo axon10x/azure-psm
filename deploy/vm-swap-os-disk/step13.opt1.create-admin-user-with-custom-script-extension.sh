7fc030f3c603e54805f15695d23f711d07092d7a
Patrick El-Azem <paelaz@microsoft.com>
2021-02-10 02:02:47 -0500

    Fix minor error

--- deploy/vm-swap-os-disk/step13.opt1.create-admin-user-with-custom-script-extension.sh
+++ deploy/vm-swap-os-disk/step13.opt1.create-admin-user-with-custom-script-extension.sh
@@ -5,9 +5,6 @@
 echo "Retrieve new Admin Username and SSH Public Key from Key Vault"
 # Note, while we defined these in step00, THAT was only to put them INTO Key Vault in step04.
 # This retrieval could equally well work if you just run this step / use your own Key Vault info.
-
-# Re-using the SSH key we previously set and used for the initial admin user
-# Obviously you can use a different one here
 vmNewAdminUsername="$(az keyvault secret show --subscription "$subscriptionId" --vault-name "$keyVaultNameLocation1" --name "$keyVaultSecretNameNewAdminUsername" -o tsv --query 'value')"
 vmNewAdminUserSshPublicKey="$(az keyvault secret show --subscription "$subscriptionId" --vault-name "$keyVaultNameLocation1" --name "$keyVaultSecretNameNewAdminSshPublicKey" -o tsv --query 'value')"
 
