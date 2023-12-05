b4a90378eb9045f84388f5ad450f48687f6fc777
Patrick El-Azem <p@elazem.com>
2023-12-05 12:41:17 -0500

    Add and organize scripts

--- scripts/AzureMonitor.ps1
+++ scripts/AzureMonitor.ps1
@@ -1,56 +1,279 @@
-function Get-DiagnosticsSettingsForSub()
+function Deploy-Ampls()
 {
   [CmdletBinding()]
   param
   (
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]
     $SubscriptionId,
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $true)]
     [string]
-    $LogAnalyticsWorkspaceId = ""
+    $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TemplateUri,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $PrivateLinkScopeName,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $QueryAccessMode = "Open",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $IngestionAccessMode = "Open",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Tags = ""
   )
-  Write-Debug -Debug:$true -Message "Get-DiagnosticsSettingsForSub $ResourceName"
 
-  [System.Collections.ArrayList]$result = @()
+  Write-Debug -Debug:$true -Message "Deploy Azure Monitor Private Link Scope $PrivateLinkScopeName"
 
-  if ($LogAnalyticsWorkspaceId)
-  {
-    $query = "(value)[?(workspaceId=='" + $LogAnalyticsWorkspaceId + "')].{name:name, id:id}"
-  }
-  else
-  {
-    $query = "(value)[].{name:name, id:id}"
-  }
+  $output = az deployment group create --verbose `
+    --subscription "$SubscriptionId" `
+    -n "$PrivateLinkScopeName" `
+    -g "$ResourceGroupName" `
+    --template-uri "$TemplateUri" `
+    --parameters `
+    location=global `
+    linkScopeName=$PrivateLinkScopeName `
+    queryAccessMode=$QueryAccessMode `
+    ingestionAccessMode=$IngestionAccessMode `
+    tags=$Tags `
+    | ConvertFrom-Json
 
-  $settings = "$(az monitor diagnostic-settings subscription list --subscription $SubscriptionId --query "$query" 2>nul)" | ConvertFrom-Json
-
-  if ($settings) { $result.Add($settings) | Out-Null }
-
-  return $result
+  return $output
 }
 
-function Remove-DiagnosticsSettingsForSub()
+function Deploy-DataCollectionEndpoint()
 {
   [CmdletBinding()]
   param
   (
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]
     $SubscriptionId,
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $true)]
     [string]
-    $LogAnalyticsWorkspaceId = ""
+    $Location,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TemplateUri,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $DataCollectionEndpointName,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PublicNetworkAccess = "Disabled",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Tags = ""
   )
-  Write-Debug -Debug:$true -Message "Remove-DiagnosticsSettingsForSub $SubscriptionId"
 
-  $settings = Get-DiagnosticsSettingsForSub -SubscriptionId $SubscriptionId -LogAnalyticsWorkspaceId $LogAnalyticsWorkspaceId
+  Write-Debug -Debug:$true -Message "Deploy Data Collection Endpoint $DataCollectionEndpointName"
 
-  foreach ($setting in $settings)
-  {
-    $dgid = "/" + $setting.id
-    az monitor diagnostic-settings subscription delete --ids $dgid --yes
-  }
+  $output = az deployment group create --verbose `
+    --subscription "$SubscriptionId" `
+    -n "$WorkspaceName" `
+    -g "$ResourceGroupName" `
+    --template-uri "$TemplateUri" `
+    --parameters `
+    location="$Location" `
+    dataCollectionEndpointName="$DataCollectionEndpointName" `
+    publicNetworkAccess="$PublicNetworkAccess" `
+    tags=$Tags `
+    | ConvertFrom-Json
+
+  return $output
+}
+
+function Deploy-DataCollectionRule()
+{
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SubscriptionId,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Location,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TemplateUri,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $DataCollectionRuleName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $LogAnalyticsWorkspaceName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $LogAnalyticsWorkspaceResourceId,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Tags = ""
+  )
+
+  Write-Debug -Debug:$true -Message "Deploy Data Collection Endpoint $DataCollectionRuleName"
+
+  $output = az deployment group create --verbose `
+    --subscription "$SubscriptionId" `
+    -n "$DataCollectionRuleName" `
+    -g "$ResourceGroupName" `
+    --template-uri "$TemplateUri" `
+    --parameters `
+    location="$Location" `
+    dataCollectionRuleName="$DataCollectionRuleName" `
+    logAnalyticsWorkspaceName="$LogAnalyticsWorkspaceName" `
+    logAnalyticsWorkspaceResourceId="$LogAnalyticsWorkspaceResourceId" `
+    tags=$Tags `
+    | ConvertFrom-Json
+
+  return $output
+}
+
+function Deploy-DiagnosticsSetting()
+{
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SubscriptionId,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TemplateUri,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceId,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $DiagnosticsSettingName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $LogAnalyticsWorkspaceResourceId,
+    [Parameter(Mandatory = $false)]
+    [bool]
+    $SendLogs = $true,
+    [Parameter(Mandatory = $false)]
+    [bool]
+    $SendMetrics = $true
+  )
+
+  Write-Debug -Debug:$true -Message "Deploy Diagnostics Setting $DiagnosticsSettingName"
+
+  $output = az deployment group create --verbose `
+    --subscription "$SubscriptionId" `
+    -n "$DiagnosticsSettingName" `
+    -g "$ResourceGroupName" `
+    --template-uri "$TemplateUri" `
+    --parameters `
+    resourceId="$ResourceId" `
+    diagnosticsSettingName="$DiagnosticsSettingName" `
+    logAnalyticsWorkspaceResourceId="$LogAnalyticsWorkspaceResourceId" `
+    sendLogs=$SendLogs `
+    sendMetrics=$SendMetrics `
+    | ConvertFrom-Json
+
+  return $output
+}
+
+function Deploy-LawToAmplsConnection()
+{
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SubscriptionId,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TemplateUri,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $PrivateLinkScopeName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ScopedResourceId,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ScopedResourceName
+  )
+
+  Write-Debug -Debug:$true -Message "Connect Log Analytics Workspace $ScopedResourceName to AMPLS $PrivateLinkScopeName"
+
+  $output = az deployment group create --verbose `
+    --subscription "$SubscriptionId" `
+    -n "$PrivateLinkScopeName-$ScopedResourceName" `
+    -g "$ResourceGroupName" `
+    --template-uri "$TemplateUri" `
+    --parameters `
+    linkScopeName=$PrivateLinkScopeName `
+    scopedResourceId=$ScopedResourceId `
+    scopedResourceName=$ScopedResourceName `
+    | ConvertFrom-Json
+
+  return $output
+}
+
+function Deploy-LogAnalyticsWorkspace() {
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SubscriptionId,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Location,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TemplateUri,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $WorkspaceName,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PublicNetworkAccessForIngestion = "Enabled",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PublicNetworkAccessForQuery = "Enabled",
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Tags = ""
+  )
+
+  Write-Debug -Debug:$true -Message "Deploy Log Analytics Workspace $WorkspaceName"
+
+  $output = az deployment group create --verbose `
+    --subscription "$SubscriptionId" `
+    -n "$WorkspaceName" `
+    -g "$ResourceGroupName" `
+    --template-uri "$TemplateUri" `
+    --parameters `
+    location="$Location" `
+    workspaceName="$WorkspaceName" `
+    publicNetworkAccessForIngestion="$PublicNetworkAccessForIngestion" `
+    publicNetworkAccessForQuery="$PublicNetworkAccessForQuery" `
+    tags=$Tags `
+    | ConvertFrom-Json
+
+  return $output
 }
 
 function Get-DiagnosticsSettingsForResource()
@@ -111,6 +334,106 @@ function Get-DiagnosticsSettingsForResource()
   return $result
 }
 
+function Get-DiagnosticsSettingsForSub()
+{
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $SubscriptionId,
+    [Parameter(Mandatory=$false)]
+    [string]
+    $LogAnalyticsWorkspaceId = ""
+  )
+  Write-Debug -Debug:$true -Message "Get-DiagnosticsSettingsForSub $ResourceName"
+
+  [System.Collections.ArrayList]$result = @()
+
+  if ($LogAnalyticsWorkspaceId)
+  {
+    $query = "(value)[?(workspaceId=='" + $LogAnalyticsWorkspaceId + "')].{name:name, id:id}"
+  }
+  else
+  {
+    $query = "(value)[].{name:name, id:id}"
+  }
+
+  $settings = "$(az monitor diagnostic-settings subscription list --subscription $SubscriptionId --query "$query" 2>nul)" | ConvertFrom-Json
+
+  if ($settings) { $result.Add($settings) | Out-Null }
+
+  return $result
+}
+
+function New-LogAnalyticsWorkspaceDataExport()
+{
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $SubscriptionId,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ResourceGroupName,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $LogAnalyticsWorkspaceName,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $DataExportName,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $DestinationResourceId,
+    [Parameter(Mandatory=$false)]
+    [string[]]
+    $TableNames = $null
+  )
+  Write-Debug -Debug:$true -Message "New-LogAnalyticsWorkspaceDataExport $LogAnalyticsWorkspaceName on $ResourceId"
+
+  if ($null -eq $TableNames -or $TableNames.Count -eq 0)
+  {
+    New-AzOperationalInsightsDataExport
+      -ResourceGroupName "$ResourceGroupName" `
+      -WorkspaceName "$LogAnalyticsWorkspaceName" `
+      -DataExportName "$DataExportName" `
+      -ResourceId "$DestinationResourceId"
+  }
+  else
+  {
+    New-AzOperationalInsightsDataExport
+      -ResourceGroupName "$ResourceGroupName" `
+      -WorkspaceName "$LogAnalyticsWorkspaceName" `
+      -DataExportName "$DataExportName" `
+      -ResourceId "$DestinationResourceId" `
+      -TableName $TableNames
+  }
+}
+
+function Remove-DiagnosticsSetting()
+{
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $SubscriptionId,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $DiagnosticsSettingName,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ResourceId,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ResourceName
+  )
+  Write-Debug -Debug:$true -Message "Remove-DiagnosticsSetting $DiagnosticsSettingName from $ResourceName"
+
+  az monitor diagnostic-settings delete --subscription $SubscriptionId --name $DiagnosticsSettingName --resource $ResourceId
+}
+
 function Remove-DiagnosticsSettingsForAllResources()
 {
   [CmdletBinding()]
@@ -168,7 +491,7 @@ function Remove-DiagnosticsSettingsForResource()
   }
 }
 
-function Remove-DiagnosticsSetting()
+function Remove-DiagnosticsSettingsForSub()
 {
   [CmdletBinding()]
   param
@@ -176,103 +499,17 @@ function Remove-DiagnosticsSetting()
     [Parameter(Mandatory=$true)]
     [string]
     $SubscriptionId,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $DiagnosticsSettingName,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $ResourceId,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $ResourceName
-  )
-  Write-Debug -Debug:$true -Message "Remove-DiagnosticsSetting $DiagnosticsSettingName from $ResourceName"
-
-  az monitor diagnostic-settings delete --subscription $SubscriptionId --name $DiagnosticsSettingName --resource $ResourceId
-}
-
-function New-DiagnosticsSetting()
-{
-  [CmdletBinding()]
-  param
-  (
-    [Parameter(Mandatory=$true)]
-    [string]
-    $SubscriptionId,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $ResourceGroupName,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $DiagnosticsSettingName,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $LogAnalyticsWorkspaceId,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $ResourceId,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $SendLogs,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $SendMetrics
-  )
-  Write-Debug -Debug:$true -Message "New-DiagnosticsSetting $DiagnosticsSettingName on $ResourceId"
-
-  az deployment group create `
-    -n "$DiagnosticsSettingName" `
-    -g "$ResourceGroupName" `
-    --template-file "./template/diagnostic-settings.json" `
-    --parameters `
-    resourceId="$ResourceId" `
-    diagnosticsSettingName="$DiagnosticsSettingName" `
-    logAnalyticsWorkspaceResourceId="$LogAnalyticsWorkspaceId" `
-    sendLogs=$SendLogs `
-    sendMetrics=$SendMetrics
-}
-
-function New-LogAnalyticsWorkspaceDataExport()
-{
-  [CmdletBinding()]
-  param
-  (
-    [Parameter(Mandatory=$true)]
-    [string]
-    $SubscriptionId,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $ResourceGroupName,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $LogAnalyticsWorkspaceName,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $DataExportName,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $DestinationResourceId,
     [Parameter(Mandatory=$false)]
-    [string[]]
-    $TableNames = $null
+    [string]
+    $LogAnalyticsWorkspaceId = ""
   )
-  Write-Debug -Debug:$true -Message "New-LogAnalyticsWorkspaceDataExport $LogAnalyticsWorkspaceName on $ResourceId"
+  Write-Debug -Debug:$true -Message "Remove-DiagnosticsSettingsForSub $SubscriptionId"
 
-  if ($TableNames -eq $null -or $TableNames.Count -eq 0)
+  $settings = Get-DiagnosticsSettingsForSub -SubscriptionId $SubscriptionId -LogAnalyticsWorkspaceId $LogAnalyticsWorkspaceId
+
+  foreach ($setting in $settings)
   {
-    New-AzOperationalInsightsDataExport
-      -ResourceGroupName "$ResourceGroupName" `
-      -WorkspaceName "$LogAnalyticsWorkspaceName" `
-      -DataExportName "$DataExportName" `
-      -ResourceId "$DestinationResourceId"
-  }
-  else
-  {
-    New-AzOperationalInsightsDataExport
-      -ResourceGroupName "$ResourceGroupName" `
-      -WorkspaceName "$LogAnalyticsWorkspaceName" `
-      -DataExportName "$DataExportName" `
-      -ResourceId "$DestinationResourceId" `
-      -TableName $TableNames
+    $dgid = "/" + $setting.id
+    az monitor diagnostic-settings subscription delete --ids $dgid --yes
   }
 }
