7e31b9075b677ab44bcc7abc7309d202749e7b9f
Patrick El-Azem <p@elazem.com>
2024-01-09 18:30:40 -0500

    Fix tags in one method

--- scripts/AzureUtility.ps1
+++ scripts/AzureUtility.ps1
@@ -1,3 +1,34 @@
+function Get-TagsForArmTemplate()
+{
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Tags = ""
+  )
+
+  Write-Debug -Debug:$true -Message "Get-TagsForArmTemplate: $Tags"
+
+  $tagsObject = @{}
+
+  if ($Tags)
+  {
+    $tagKVPairs = $Tags.Split(",")
+    foreach ($tagKVPair in $tagKVPairs)
+    {
+      $tagKVArray = $tagKVPair.Split("=")
+      $tagsObject[$tagKVArray[0]] = $tagKVArray[1]
+    }
+  }
+
+  $tagsForArm = ConvertTo-Json -InputObject $tagsObject -Compress
+  $tagsForArm = $tagsForArm.Replace('"', '''')
+  $tagsForArm = "`"$tagsForArm`""
+
+  return $tagsForArm
+}
+
 function Remove-AzPackages()
 {
   Get-Package | Where-Object { $_.Name -like 'Az*' } | ForEach-Object { Uninstall-Package -Name $_.Name -AllVersions }
