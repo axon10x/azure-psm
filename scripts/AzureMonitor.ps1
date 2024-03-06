67150249afa11fa0c536b25096f1d4300031fff8
Patrick El-Azem <p@elazem.com>
2024-03-05 23:10:35 -0500

    Fix bug

--- scripts/AzureMonitor.ps1
+++ scripts/AzureMonitor.ps1
@@ -166,8 +166,9 @@ function Deploy-DiagnosticsSetting()
     sendAllLogs=$SendAllLogs `
     sendAuditLogs=$SendAuditLogs `
     sendMetrics=$SendMetrics `
-    2>nul `
     | ConvertFrom-Json
+
+    # 2>nul `
 
   if (!$output)
   {
